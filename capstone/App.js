1ed3ae0175ee978ada8265cbaeb3bde454b4ed05
SCR08 <f1challenge2015@hotmail.com>
2023-06-06 15:49:29 -0500

    git commit -m'Final commit'

---
+++ capstone/App.js
@@ -0,0 +1,49 @@
+import { View, StyleSheet } from 'react-native';
+import LittleLemonHeader from './components/LittleLemonHeader';
+import LittleLemonFooter from './components/LittleLemonFooter';
+import MenuScreen from './components/Menu';
+import WelcomeScreen from './components/WelcomeScreen';
+import LoginScreen from './components/LoginScreen';
+import SubscribeScreen from './components/SubscribeScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+//import { BottomTabBarHeightContext, createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+//import { createDrawerNavigator } from '@react-navigation/drawer';
+import { Ionicons } from '@expo/vector-icons';
+
+
+const Stack = createNativeStackNavigator();
+//const Tab = createBottomTabNavigator();
+//const Drawer = createDrawerNavigator();
+
+
+
+
+export default function App() {
+  return (
+    <NavigationContainer>
+        <View style={styles.container}>
+          <LittleLemonHeader />
+          <Stack.Navigator initialRouteName="Login">
+            <Stack.Screen name="Welcome" component={WelcomeScreen} />
+            <Stack.Screen name="Login" component={LoginScreen} />
+            <Stack.Screen name="Menu" component={MenuScreen} />
+            <Stack.Screen name="Subscribe" component={SubscribeScreen} />
+          </Stack.Navigator>
+        </View>
+        <View style={styles.footerContainer}>
+          <LittleLemonFooter />
+        </View>
+      </NavigationContainer>
+  );
+}
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#495E57',
+  },
+  footerContainer:{ backgroundColor: '#333333' }
+})
